51b364722dc336443f910e7f4770b2b19ea451b1
ronaldleung1 <ronaldfleung@gmail.com>
2023-10-07 15:58:46 -0400

    complete GET /api/posts/

--- pa1/README.txt
+++ pa1/README.txt
@@ -1,5 +1,5 @@
-Name:
-NetID:
+Name: Ronald Leung
+NetID: rfl68
 
 Challenges Attempted: (Tier I, Tier II, Tier III)
 
--- pa1/src/app.py
+++ pa1/src/app.py
@@ -6,10 +6,31 @@ from flask import request
 
 app = Flask(__name__)
 
+posts = {
+    0: {
+      "id": 0,
+      "upvotes": 1,
+      "title": "My cat is the cutest!",
+      "link": "https://i.imgur.com/jseZqNK.jpg",
+      "username": "alicia98",
+    },
+    1: {
+      "id": 1,
+      "upvotes": 3,
+      "title": "Cat loaf",
+      "link": "https://i.imgur.com/TJ46wX4.jpg",
+      "username": "alicia98",
+    }
+}
 
 @app.route("/")
 def hello_world():
     return "Hello world!"
+
+@app.route("/api/posts", methods=["GET"])
+def get_posts():
+    res = {"posts": list(posts.values())}
+    return json.dumps(res), 200
 
 
 # your routes here

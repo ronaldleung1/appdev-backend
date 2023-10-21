fbbd34f459432a51b015d225c852b0a900e4563d
ronaldleung1 <ronaldfleung@gmail.com>
2023-10-20 22:50:22 -0400

    complete crud for task list database

--- lec2/src/app.py
+++ lec2/src/app.py
@@ -1,6 +1,9 @@
 import json
 from flask import Flask, request
 
+import db
+DB = db.DatabaseDriver()
+
 app = Flask(__name__)
 
 
@@ -8,27 +11,64 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/tasks/")
 def get_tasks():
-    pass
+    """
+    Endpoint for getting all tasks
+    """
+    return json.dumps({"tasks": DB.get_all_tasks()})
 
 
 @app.route("/tasks/", methods=["POST"])
 def create_task():
-    pass
+    """
+    Endpoint for creating new task
+    """
+    body = json.loads(request.data)
+    description = body.get("description")
+    task_id = DB.insert_task_table(description, False)
+    task = DB.get_task_by_id(task_id)
+    if task is None:
+        return json.dumps({"error": "Something went wrong while making task"}), 400
+    return json.dumps(task), 201
 
 
 @app.route("/tasks/<int:task_id>/")
 def get_task(task_id):
-    pass
+    """
+    Endpoint for getting a task by ID
+    """
+
+    task = DB.get_task_by_id(task_id)
+    if task is None:
+        return json.dumps({"error": "Task not found"}), 404
+    return json.dumps(task), 200
 
 
 @app.route("/tasks/<int:task_id>/", methods=["POST"])
 def update_task(task_id):
-    pass
+    """
+    Endpoint for updating a task by ID
+    """
+
+    body = json.loads(request.data)
+    description = body.get("description")
+    done = bool(body.get("done"))
+
+    DB.update_task_by_id(task_id, description, done)
+
+    task = DB.get_task_by_id(task_id)
+    if task is None:
+        return json.dumps({"error": "Task not found"}), 404
+    return json.dumps(task), 200
+
 
 
 @app.route("/tasks/<int:task_id>/", methods=["DELETE"])
 def delete_task(task_id):
-    pass
+    task = DB.get_task_by_id(task_id)
+    if task is None:
+        return json.dumps({"error": "Task not found"}), 404
+    DB.delete_task_by_id(task_id)
+    return json.dumps(task), 200
 
 
 if __name__ == "__main__":
--- lec2/src/db.py
+++ lec2/src/db.py
@@ -20,8 +20,101 @@ class DatabaseDriver(object):
     """
 
     def __init__(self):
-        pass
+        """
+        Secures a connection with the database and stores it in an instance variable
+        """
+        
+        self.conn = sqlite3.connect(
+            "todo.db", check_same_thread=False
+        )
 
+        # self.delete_task_table()
+        self.create_task_table()
+
+    def create_task_table(self):
+        """
+        Using SQL, create task table
+        """
+
+        try:
+            self.conn.execute(
+                """
+                CREATE TABLE task (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    description TEXT NOT NULL,
+                    done BOOLEAN NOT NULL
+                );
+                """
+            )
+        except Exception as e:
+            print(e)
+
+    def delete_task_table(self):
+        """
+        Using SQL, deletes task table
+        """
+
+        self.conn.execute("DROP TABLE IF EXISTS task;")
+    
+    def get_all_tasks(self):
+        """
+        Using SQL, get all tasks in the task table
+        """
+        cursor = self.conn.execute("SELECT * FROM task;")
+        tasks = []
+
+        for row in cursor:
+            tasks.append({"id": row[0], "description": row[1], "done": bool(row[2])})
+        
+        return tasks
+    
+    def insert_task_table(self, description, done):
+        """
+        Using SQL, adds a new taks in the task table
+        """
+        cursor = self.conn.execute("INSERT INTO task (description, done) VALUES (?, ?);", (description, done))
+        self.conn.commit()
+        return cursor.lastrowid
+    
+    def get_task_by_id(self, id):
+        """
+        Using SQL, get a task by ID
+        """
+        cursor = self.conn.execute("SELECT * FROM task WHERE ID = ?;", (id,))
+
+        for row in cursor:
+            return({"id": row[0], "description": row[1], "done": bool(row[2])})
+
+        return None
+    
+    def update_task_by_id(self, id, description, done):
+        """
+        Using SQL, update a task by ID
+        """
+        
+        self.conn.execute(
+            """
+            UPDATE task
+            SET description = ?, done = ?
+            WHERE id = ?;
+            """,
+            (description, done, id)
+        )
+        self.conn.commit()
+    
+    def delete_task_by_id(self, id):
+        """
+        Using SQL, deletes a task by id
+        """
+
+        self.conn.execute(
+            """
+            DELETE FROM task
+            WHERE id = ?;
+            """,
+            (id,)
+        )
+        self.conn.commit()
 
 # Only <=1 instance of the database driver
 # exists within the app at all times

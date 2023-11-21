3093b3e5acc98bce35073de6a16cb7ab21cc008f
ronaldleung1 <ronaldfleung@gmail.com>
2023-11-21 14:54:33 -0500

    add starter for pa6

---
+++ pa6/README.txt
@@ -0,0 +1,24 @@
+Name: Ronald Leung
+NetID: rfl68
+
+Working Endpoint: GET /api/courses/
+Your Server Address: 
+
+Questions:
+Explain the concept of deployment in your own words.
+<Your answer here>
+
+What are environment variables?
+<Your answer here>
+
+What is the filename of the file where environment variables are traditionally stored?
+<Your answer here>
+
+What is the network protocol we use to access servers?
+<Your answer here>
+
+Explain the concept of clustering in your own words.
+<Your answer here>
+
+Explain the concept of load balancing in your own words.
+<Your answer here>
---
+++ pa6/src/app.py
@@ -0,0 +1,41 @@
+import json
+
+from db import db
+from flask import Flask, request
+from db import Course
+
+app = Flask(__name__)
+db_filename = "cms.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+# your routes here
+@app.route("/api/courses/", methods=["GET"])
+def get_courses():
+    """
+    Endpoint for getting all courses
+    """
+    courses = [course.serialize() for course in Course.query.all()]
+    return json.dumps({"courses": courses}), 200
+
+@app.route("/api/courses/", methods=["POST"])
+def create_course():
+    """
+    Endpoint for creating a course
+    """
+    body = json.loads(request.data)
+    new_course = Course(code=body.get("code"), name=body.get("name"))
+    db.session.add(new_course)
+    db.session.commit()
+    return json.dumps(new_course.serialize()), 201
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
